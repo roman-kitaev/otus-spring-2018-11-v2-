43975c44aed6c740fe802d4e5d4c29c949b570ad
relok <ritter88@yandex.ru>
2019-04-17 18:13:07 +0300

    small cleanup

--- HW141/src/main/java/ru/otus/HW141/Hw141Application.java
+++ HW141/src/main/java/ru/otus/HW141/Hw141Application.java
@@ -3,13 +3,15 @@ package ru.otus.HW141;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import ru.otus.HW141.domain.Book;
+import ru.otus.HW141.repostory.BookRepository;
 
 @SpringBootApplication
 public class Hw141Application {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Hw141Application.class, args);
 
-  		/*BookRepository bookRepository = context.getBean(BookRepository.class);
+/*  		BookRepository bookRepository = context.getBean(BookRepository.class);
 
 		for(int i = 0; i < 10; i++) {
 			bookRepository.save(new Book("Boook" + (i + 1)));
