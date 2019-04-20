8aff03f40668fab9264b8ab64d4e726229c053e2
relok <ritter88@yandex.ru>
2019-04-20 13:04:50 +0300

    the test is working

--- HW121/src/test/java/ru/otus/HW121/page/BooksPagesControllerTest.java
+++ HW121/src/test/java/ru/otus/HW121/page/BooksPagesControllerTest.java
@@ -1,29 +1,41 @@
 package ru.otus.HW121.page;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(BooksPagesController.class)
+@SpringBootTest
 public class BooksPagesControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
 
-    @WithMockUser(
-            username = "admin",
-            authorities = {"admin"}
-    )
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser()
     @Test
     public void testAuthenticatedOnAdmin() throws Exception {
-        mockMvc.perform(get("/admin"))
+        mvc.perform(get("/admin"))
                 .andExpect(status().isOk());
     }
 }
